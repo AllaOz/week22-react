13ee625865175955094679d5233e8edfa4fca263
AllaOz <ozcanalla@gmail.com>
2023-06-11 17:42:12 +0300

    exlint

--- my-app/src/components/Heroes.jsx
+++ my-app/src/components/Heroes.jsx
@@ -1,17 +1,17 @@
 
 import './Heroes.css';
 
-  function Heroes(props) {
+function Heroes(props) {
     return (
-      <div className="hero-card">
-        <h2>{props.name}</h2>
-        <div>Альтер эго: {props.alterego}</div>
-        <div>Занятия: {props.occupation}</div>
-        <div>Друзья: {props.friends}</div>
-        <div>Суперсила: {props.superpowers}</div>
-        <img className="image" src={props.url} alt="комикс фильма" />
-      </div>
+        <div className="hero-card">
+            <h2>{props.name}</h2>
+            <div>Альтер эго: {props.alterego}</div>
+            <div>Занятия: {props.occupation}</div>
+            <div>Друзья: {props.friends}</div>
+            <div>Суперсила: {props.superpowers}</div>
+            <img className="image" src={props.url} alt="комикс фильма" />
+        </div>
     );
-  }
-  export default Heroes;
+}
+export default Heroes;
   
